0673bf701e5d15aa9de849bc3452721fdb334f10
Jose Montoya <jam01@protonmail.com>
2020-12-19 15:10:58 -0600

    refactor roles to fix dry-run integration tests
    - refactored postgresql, kong, and keycloak password generation to use
    provided passwords if given in order to enable assertions on generated
    manifests
    - added generated manifests to run assertion using diff
    - generated passwords are now added back to the given config and the
    config is saved to a file at the end of the playbook
    - refactored secret generation to not use kubectl to enable dry-run test
    - added a test for kong-ee config

---
+++ tests/integration/targets/playbooks/provision_playbook/example.com/platform/postgresql/setup.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE 'gitea';
+CREATE USER 'gitea' WITH ENCRYPTED PASSWORD 'gitea';
+GRANT ALL PRIVILEGES ON DATABASE 'gitea' TO 'gitea';
+
+CREATE DATABASE 'keycloak';
+CREATE USER 'keycloak' WITH ENCRYPTED PASSWORD 'placeholder3';
+GRANT ALL PRIVILEGES ON DATABASE 'keycloak' TO 'keycloak';
+
+CREATE DATABASE 'sandbox-kong';
+CREATE USER 'sandbox-kong' WITH ENCRYPTED PASSWORD 'placeholder1';
+GRANT ALL PRIVILEGES ON DATABASE 'sandbox-kong' TO 'sandbox-kong';
+
+CREATE DATABASE 'prod-kong';
+CREATE USER 'prod-kong' WITH ENCRYPTED PASSWORD 'placeholder2';
+GRANT ALL PRIVILEGES ON DATABASE 'prod-kong' TO 'prod-kong';
+
