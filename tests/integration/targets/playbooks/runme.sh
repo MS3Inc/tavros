c6f78b9ac527801467b4cb671f072aca0328485f
rmccright-ms3 <rmccright@ms3-inc.com>
2021-01-22 11:27:41 -0500

    Updating Test for last Kong - Updated runme.sh to ignore .DS_STORE files - Update Example templates

--- tests/integration/targets/playbooks/runme.sh
+++ tests/integration/targets/playbooks/runme.sh
@@ -12,10 +12,10 @@ echo "--- validating provision playbook with default config"
 
 rm -rf /tmp/example.com/*
 ansible-playbook $PLAYBOOKS_PATH/provision_playbook.yaml --extra-vars '{"cluster_name":"tavros","cluster_domain":"example.com","cluster_admin_email":"ops@example.com"}' --inventory "./provision_playbook/default_vars.yaml" --tags all,dry-run
-diff --recursive /tmp/example.com/ ./provision_playbook/example.com/
+diff --recursive --exclude='.DS_Store' /tmp/example.com/ ./provision_playbook/example.com/
 
 echo "--- validating provision playbook with kong ee config"
 
 rm -rf /tmp/enterprise-example.com/*
 ansible-playbook $PLAYBOOKS_PATH/provision_playbook.yaml --extra-vars '{"cluster_name":"tavros","cluster_domain":"enterprise-example.com","cluster_admin_email":"ops@example.com"}' --inventory "./provision_playbook/kong_ee_vars.yaml" --tags all,dry-run
-diff --recursive --exclude='ee-session-secret.yaml' --exclude='*.gitkeep' /tmp/enterprise-example.com/ ./provision_playbook/enterprise-example.com/
+diff --recursive --exclude='.DS_Store' --exclude='ee-session-secret.yaml' --exclude='*.gitkeep' /tmp/enterprise-example.com/ ./provision_playbook/enterprise-example.com/
