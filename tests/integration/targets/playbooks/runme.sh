0b3e41636ff96a234420a5ddbf16d99d7e676141
Jose Montoya <jam01@protonmail.com>
2021-02-17 18:38:20 -0600

    change cluster name, domain for fqdn

--- tests/integration/targets/playbooks/runme.sh
+++ tests/integration/targets/playbooks/runme.sh
@@ -11,11 +11,11 @@ PLAYBOOKS_PATH=$ANSIBLE_COLLECTIONS_PATH/ansible_collections/ms3_inc/tavros/play
 echo "--- validating provision playbook with default config"
 
 rm -rf /tmp/example.com/*
-ansible-playbook $PLAYBOOKS_PATH/provision_playbook.yaml --extra-vars '{"cluster_name":"tavros","cluster_domain":"example.com","cluster_admin_email":"ops@example.com"}' --inventory "./provision_playbook/default_vars.yaml" --tags all,dry-run
-diff --recursive --exclude='.DS_Store' /tmp/example.com/ ./provision_playbook/example.com/
+ansible-playbook $PLAYBOOKS_PATH/provision_playbook.yaml --extra-vars '{"cluster_fqdn":"tavros.example.com","cluster_admin_email":"ops@example.com"}' --inventory "./provision_playbook/default_vars.yaml" --tags all,dry-run
+diff --recursive /tmp/tavros.example.com/ ./provision_playbook/example.com/
 
 echo "--- validating provision playbook with kong ee config"
 
 rm -rf /tmp/enterprise-example.com/*
-ansible-playbook $PLAYBOOKS_PATH/provision_playbook.yaml --extra-vars '{"cluster_name":"tavros","cluster_domain":"enterprise-example.com","cluster_admin_email":"ops@example.com"}' --inventory "./provision_playbook/kong_ee_vars.yaml" --tags all,dry-run
-diff --recursive --exclude='.DS_Store' --exclude='ee-session-secret.yaml' --exclude='*.gitkeep' /tmp/enterprise-example.com/ ./provision_playbook/enterprise-example.com/
+ansible-playbook $PLAYBOOKS_PATH/provision_playbook.yaml --extra-vars '{"cluster_fqdn":"tavros.enterprise-example.com","cluster_admin_email":"ops@example.com"}' --inventory "./provision_playbook/kong_ee_vars.yaml" --tags all,dry-run
+diff --recursive --exclude='ee-session-secret.yaml' --exclude='*.gitkeep' /tmp/tavros.enterprise-example.com/ ./provision_playbook/enterprise-example.com/
