1ae0709708cd43b10cd1864aa4086b0196ae51be
Jose Montoya <jam01@protonmail.com>
2021-03-18 14:00:05 -0500

    reintroduce dry-run tag support

--- tests/integration/targets/playbooks/runme.sh
+++ tests/integration/targets/playbooks/runme.sh
@@ -11,11 +11,11 @@ PLAYBOOKS_PATH=$ANSIBLE_COLLECTIONS_PATH/ansible_collections/ms3_inc/tavros/play
 echo "--- validating provision playbook with default config"
 
 rm -rf /tmp/tavros.example.com/*
-ansible-playbook $PLAYBOOKS_PATH/provision_playbook.yaml --extra-vars '{"cluster_fqdn":"tavros.example.com","cluster_admin_email":"ops@example.com"}' --inventory "./provision_playbook/default_vars.yaml" --skip-tags requires_cluster
+ansible-playbook $PLAYBOOKS_PATH/provision_playbook.yaml --extra-vars '{"cluster_fqdn":"tavros.example.com","cluster_admin_email":"ops@example.com"}' --inventory "./provision_playbook/default_vars.yaml" --tags all,dry-run
 diff --recursive /tmp/tavros.example.com/ ./provision_playbook/example.com/
 
 echo "--- validating provision playbook with kong ee config"
 
 rm -rf /tmp/tavros.enterprise-example.com/*
-ansible-playbook $PLAYBOOKS_PATH/provision_playbook.yaml --extra-vars '{"cluster_fqdn":"tavros.enterprise-example.com","cluster_admin_email":"ops@example.com"}' --inventory "./provision_playbook/kong_ee_vars.yaml" --skip-tags requires_cluster
+ansible-playbook $PLAYBOOKS_PATH/provision_playbook.yaml --extra-vars '{"cluster_fqdn":"tavros.enterprise-example.com","cluster_admin_email":"ops@example.com"}' --inventory "./provision_playbook/kong_ee_vars.yaml" --tags all,dry-run
 diff --recursive --exclude='secret-ee-admin-gui-session.yaml' --exclude='*.gitkeep' /tmp/tavros.enterprise-example.com/ ./provision_playbook/enterprise-example.com/
