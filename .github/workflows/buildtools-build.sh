08c21120423472f10b68847c20507b04f9aaa402
Jose Montoya <jam01@users.noreply.github.com>
2021-06-18 15:17:32 -0500

    Update buildtools-build.sh

--- .github/workflows/buildtools-build.sh
+++ .github/workflows/buildtools-build.sh
@@ -48,7 +48,7 @@ podman exec installer bash -c "rpm --import https://packages.microsoft.com/keys/
 podman cp ./.github/workflows/azure-cli.repo installer:/etc/yum.repos.d/azure-cli.repo
 
 podman exec installer bash -c "yum upgrade -y --quiet"
-podman exec installer bash -c "yum install pip -y --quiet --setopt tsflags=nodocs"
+podman exec installer bash -c "yum install pip -y --quiet"
 
 printf "\nInstalling tools with package manager...\n"
 dnf_install "vi make curl telnet openssl bind-utils diffutils python awscli git jq azure-cli procps nano traceroute iputils iproute"
